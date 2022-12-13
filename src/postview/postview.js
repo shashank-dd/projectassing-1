6593a84809d482b06ca9ce421ec00449326454e1
shashank-dd <119873380+shashank-dd@users.noreply.github.com>
2022-12-14 02:25:06 +0530

    Update postview.js

--- src/postview/postview.js
+++ src/postview/postview.js
@@ -44,35 +44,7 @@ function Preview(){
     return<>
 
     <div className="papa">
-//         <nav>
-//         <p className="ico">instaclone</p>
-//         <i className="fa-solid fa-camera"></i>
-//         </nav>
-//        <div className="grandpa">
-//        {data&& data.map((elem,index)=>{
-//            return <section className="card" key={index}>
-//               <div className="hjk">
-//                 <span>
-//                  <p className="ana">{elem.name}</p>  
-//                  <p className="ana">{elem.location}</p> 
-//                 </span>
-//                 <span className="gh">...</span>
-//               </div>
-//             <img src={elem.PostImage} alt="img1"/>
-//             <div className="likecon">
-//                 <div id="jh">
-//                 <i className="fa-regular fa-heart"></i>
-//                 <i className="fa-solid fa-rocket"></i>
-//                 </div>
-//                 <div id="kh">{elem.date}h</div>
-//             </div>
-            
-//               <div>
-//                 <p className="bn">{elem.description}</p>
-//               </div>
-
-//         </section>})}
-//        </div>
+     
         <p>{data}</p>
         
     </div>
